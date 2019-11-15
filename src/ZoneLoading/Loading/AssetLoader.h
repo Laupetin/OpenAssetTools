08d0dda4f66f9748921b8c46ff740aa5bf4b5c8d
Jan <jan@laupetin.net>
2019-11-15 14:53:43 +0100

    ZoneCodeGenerator: Make scriptstringprovider available to child classes of asset loader to be able to instantiate other loaders

--- src/ZoneLoading/Loading/AssetLoader.h
+++ src/ZoneLoading/Loading/AssetLoader.h
@@ -7,12 +7,13 @@ class AssetLoader : public ContentLoader
 {
     asset_type_t m_asset_type;
 
-    IZoneScriptStringProvider* m_script_string_provider;
     std::vector<std::string> m_used_script_strings;
 
     std::vector<XAssetDependency> m_dependencies;
 
 protected:
+    IZoneScriptStringProvider* m_script_string_provider;
+
     AssetLoader(asset_type_t assetType, IZoneScriptStringProvider* scriptStringProvider, Zone* zone, IZoneInputStream* stream);
     
     void AddDependency(asset_type_t type, std::string& name);
