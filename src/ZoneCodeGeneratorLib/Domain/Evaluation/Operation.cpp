9578a24c49f62a8bc188686fb9fb754872267f22
Jan <jan@laupetin.net>
2021-02-28 17:04:39 +0100

    Fix ZCG new operation 2 needs parenthesis if precendence is equal

--- src/ZoneCodeGeneratorLib/Domain/Evaluation/Operation.cpp
+++ src/ZoneCodeGeneratorLib/Domain/Evaluation/Operation.cpp
@@ -36,5 +36,5 @@ bool Operation::Operand1NeedsParenthesis() const
 bool Operation::Operand2NeedsParenthesis() const
 {
     return m_operand2->GetType() == EvaluationType::OPERATION
-        && dynamic_cast<Operation*>(m_operand2.get())->m_operation_type->m_precedence > m_operation_type->m_precedence;
+        && dynamic_cast<Operation*>(m_operand2.get())->m_operation_type->m_precedence >= m_operation_type->m_precedence;
 }
