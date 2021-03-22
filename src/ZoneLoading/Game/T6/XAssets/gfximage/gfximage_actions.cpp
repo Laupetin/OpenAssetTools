6f6d25da4acbd602608a50902337c5e4ff7a9800
Jan <jan@laupetin.net>
2021-03-22 12:30:02 +0100

    Always duplicate temp loaddef for T6 instead of only duplicating it if resource size is bigger than 0

--- src/ZoneLoading/Game/T6/XAssets/gfximage/gfximage_actions.cpp
+++ src/ZoneLoading/Game/T6/XAssets/gfximage/gfximage_actions.cpp
@@ -17,15 +17,16 @@ void Actions_GfxImage::OnImageLoaded(GfxImage* image) const
 
 void Actions_GfxImage::LoadImageData(GfxImageLoadDef* loadDef, GfxImage* image) const
 {
-    if(loadDef->resourceSize > 0)
-    {
-        const size_t loadDefSize = offsetof(T6::GfxImageLoadDef, data) + loadDef->resourceSize;
+    // if(loadDef->resourceSize > 0)
+    // {
+    //     
+    // }
+    // else
+    // {
+    //     image->texture.loadDef = nullptr;
+    // }
+    const size_t loadDefSize = offsetof(T6::GfxImageLoadDef, data) + loadDef->resourceSize;
 
-        image->texture.loadDef = static_cast<GfxImageLoadDef*>(m_zone->GetMemory()->Alloc(loadDefSize));
-        memcpy(image->texture.loadDef, loadDef, loadDefSize);
-    }
-    else
-    {
-        image->texture.loadDef = nullptr;
-    }
+    image->texture.loadDef = static_cast<GfxImageLoadDef*>(m_zone->GetMemory()->Alloc(loadDefSize));
+    memcpy(image->texture.loadDef, loadDef, loadDefSize);
 }
