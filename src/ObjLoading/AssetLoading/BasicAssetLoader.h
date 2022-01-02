8e2b9f4a62b8bd31f3fa9adc241b6bfd3f93b553
Jan <jan@laupetin.net>
2022-01-02 01:10:57 +0100

    Update iw4 asset loader clipmap to use interface without asset_type_t specification

--- src/ObjLoading/AssetLoading/BasicAssetLoader.h
+++ src/ObjLoading/AssetLoading/BasicAssetLoader.h
@@ -2,17 +2,22 @@
 #include "IAssetLoader.h"
 #include "Pool/GlobalAssetPool.h"
 
+template<typename T>
+class BasicAssetLoaderWithoutType : public IAssetLoader
+{
+public:
+    _NODISCARD XAssetInfoGeneric* LoadFromGlobalAssetPools(const std::string& assetName) const override
+    {
+        return GlobalAssetPool<T>::GetAssetByName(assetName);
+    }
+};
+
 template<asset_type_t AssetType, typename T>
-class BasicAssetLoader : public IAssetLoader
+class BasicAssetLoader : public BasicAssetLoaderWithoutType<T>
 {
 public:
     _NODISCARD asset_type_t GetHandlingAssetType() const override
     {
         return AssetType;
-    }
-
-    _NODISCARD XAssetInfoGeneric* LoadFromGlobalAssetPools(const std::string& assetName) const override
-    {
-        return GlobalAssetPool<T>::GetAssetByName(assetName);
     }
 };
--- src/ObjLoading/Game/IW4/AssetLoaders/AssetLoaderClipMap.h
+++ src/ObjLoading/Game/IW4/AssetLoaders/AssetLoaderClipMap.h
@@ -6,7 +6,7 @@
 
 namespace IW4
 {
-    class AssetLoaderClipMap : public BasicAssetLoader<ASSET_TYPE_CLIPMAP_MP, clipMap_t>
+    class AssetLoaderClipMap : public BasicAssetLoaderWithoutType<clipMap_t>
     {
         _NODISCARD void* CreateEmptyAsset(const std::string& assetName, MemoryManager* memory) override;
     };
