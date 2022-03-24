935e6ac060ba741be54dd81cadd034598f5f8e14
Jan <jan@laupetin.net>
2022-03-24 17:36:12 +0100

    Dump IW4 vertex and pixel shader binary data

--- src/ObjWriting/Game/IW4/AssetDumpers/AssetDumperPixelShader.cpp
+++ src/ObjWriting/Game/IW4/AssetDumpers/AssetDumperPixelShader.cpp
@@ -1,5 +1,7 @@
 #include "AssetDumperPixelShader.h"
 
+#include <sstream>
+
 using namespace IW4;
 
 bool AssetDumperPixelShader::ShouldDump(XAssetInfo<MaterialPixelShader>* asset)
@@ -9,4 +11,15 @@ bool AssetDumperPixelShader::ShouldDump(XAssetInfo<MaterialPixelShader>* asset)
 
 void AssetDumperPixelShader::DumpAsset(AssetDumpingContext& context, XAssetInfo<MaterialPixelShader>* asset)
 {
+    const auto* pixelShader = asset->Asset();
+
+    std::ostringstream ss;
+    ss << "shader_bin/ps_" << pixelShader->name << ".cso";
+
+    const auto shaderFile = context.OpenAssetFile(ss.str());
+
+    if (!shaderFile)
+        return;
+
+    shaderFile->write(reinterpret_cast<const char*>(pixelShader->prog.loadDef.program), static_cast<std::streamsize>(pixelShader->prog.loadDef.programSize) * 4u);
 }
--- src/ObjWriting/Game/IW4/AssetDumpers/AssetDumperVertexShader.cpp
+++ src/ObjWriting/Game/IW4/AssetDumpers/AssetDumperVertexShader.cpp
@@ -1,5 +1,7 @@
 #include "AssetDumperVertexShader.h"
 
+#include <sstream>
+
 using namespace IW4;
 
 bool AssetDumperVertexShader::ShouldDump(XAssetInfo<MaterialVertexShader>* asset)
@@ -9,4 +11,15 @@ bool AssetDumperVertexShader::ShouldDump(XAssetInfo<MaterialVertexShader>* asset
 
 void AssetDumperVertexShader::DumpAsset(AssetDumpingContext& context, XAssetInfo<MaterialVertexShader>* asset)
 {
+    const auto* vertexShader = asset->Asset();
+
+    std::ostringstream ss;
+    ss << "shader_bin/vs_" << vertexShader->name << ".cso";
+
+    const auto shaderFile = context.OpenAssetFile(ss.str());
+
+    if (!shaderFile)
+        return;
+
+    shaderFile->write(reinterpret_cast<const char*>(vertexShader->prog.loadDef.program), static_cast<std::streamsize>(vertexShader->prog.loadDef.programSize) * 4u);
 }
