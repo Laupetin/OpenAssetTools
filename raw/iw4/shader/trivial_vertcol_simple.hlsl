14846184813ea626e99924bff566978b04d254da
Jan <jan@laupetin.net>
2022-09-16 23:45:53 +0200

    Add HLSL source version for IW4 2D shaders

---
+++ raw/iw4/shader/trivial_vertcol_simple.hlsl
@@ -0,0 +1,34 @@
+struct VSInput
+{
+	float3 position : POSITION;
+    half4 color : COLOR0;
+    half2 texcoord : TEXCOORD0;
+};
+
+struct VSOutput
+{
+	float4 position : POSITION;
+	half4 color : COLOR0;
+    half2 texcoord : TEXCOORD0;
+};
+
+extern float4x4 viewProjectionMatrix;
+extern float4x4 worldMatrix;
+
+VSOutput VSMain(VSInput vin)
+{
+	VSOutput vout = (VSOutput)0;
+
+	vout.position = mul(mul(float4(vin.position, 1.0f), worldMatrix), viewProjectionMatrix);
+	vout.color = vin.color;
+	vout.texcoord = vin.texcoord;
+
+	return vout;
+}
+
+extern sampler2D colorMapSampler;
+
+half4 PSMain(VSOutput input) : SV_TARGET
+{
+    return half4(tex2D(colorMapSampler, input.texcoord)) * input.color;
+}
