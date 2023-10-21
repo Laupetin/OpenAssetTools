2c09cc11e99a34f3f84165e823eb4b730f2ea577
Jan <jan@laupetin.net>
2023-10-21 12:27:47 +0200

    Add example project for building an IPak with corresponding ff

---
+++ docs/example/IPakZone/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# You can now either build the fastfile by building the project itself
+Linker IPakZone
+
+# ...or by building both targets separately
+Linker "IPakZone/IPakZone_ff" "IPakZone/IPakZone_ipak"
