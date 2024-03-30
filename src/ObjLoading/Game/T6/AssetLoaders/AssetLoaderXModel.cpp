abc3003b5b4d54946d05ac9326fc20f270a44d4e
Jan <jan@laupetin.net>
2024-03-30 14:09:58 +0100

    chore: implement base loading and writing of xmodel json

---
+++ src/ObjLoading/Game/T6/AssetLoaders/AssetLoaderXModel.cpp
@@ -0,0 +1,44 @@
+#include "AssetLoaderXModel.h"
+
+#include "Game/T6/XModel/JsonXModelLoader.h"
+#include "Game/T6/T6.h"
+#include "Pool/GlobalAssetPool.h"
+
+#include <cstring>
+#include <format>
+#include <iostream>
+
+using namespace T6;
+
+void* AssetLoaderXModel::CreateEmptyAsset(const std::string& assetName, MemoryManager* memory)
+{
+    auto* xmodel = memory->Create<XModel>();
+    memset(xmodel, 0, sizeof(XModel));
+    xmodel->name = memory->Dup(assetName.c_str());
+
+    return xmodel;
+}
+
+bool AssetLoaderXModel::CanLoadFromRaw() const
+{
+    return true;
+}
+
+bool AssetLoaderXModel::LoadFromRaw(
+    const std::string& assetName, ISearchPath* searchPath, MemoryManager* memory, IAssetLoadingManager* manager, Zone* zone) const
+{
+    const auto file = searchPath->Open(std::format("xmodel/{}.json", assetName));
+    if (!file.IsOpen())
+        return false;
+
+    auto* xmodel = memory->Alloc<XModel>();
+    xmodel->name = memory->Dup(assetName.c_str());
+
+    std::vector<XAssetInfoGeneric*> dependencies;
+    if (LoadXModelAsJson(*file.m_stream, *xmodel, memory, manager, dependencies))
+        manager->AddAsset<AssetXModel>(assetName, xmodel, std::move(dependencies));
+    else
+        std::cerr << "Failed to load xmodel \"" << assetName << "\"\n";
+
+    return true;
+}
--- src/ObjWriting/Model/XModel/XModelExportWriter.cpp
+++ src/ObjWriting/Model/XModel/XModelExportWriter.cpp
@@ -2,6 +2,7 @@
 
 #include "Math/Quaternion.h"
 
+#include <chrono>
 #include <iomanip>
 #include <iostream>
 
