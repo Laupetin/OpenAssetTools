4d0096b634f9bb4216cd179c2201a9c9992a5fcc
Jan <jan@laupetin.net>
2024-08-08 19:22:06 +0200

    chore: include better diagnostics in json error messages

--- thirdparty/json.lua
+++ thirdparty/json.lua
@@ -2,6 +2,9 @@ json = {}
 
 function json:include(includes)
 	if includes:handle(self:name()) then
+		defines {
+			"JSON_DIAGNOSTICS=1"
+		}
 		includedirs {
 			path.join(ThirdPartyFolder(), "json", "single_include")
 		}
