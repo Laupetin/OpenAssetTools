d4d8e83169c76389e8dd0ace59f47a78655c1c3e
Jan <jan@laupetin.net>
2024-09-22 16:59:56 +0200

    feat: load iw5 images from raw

--- src/ObjLoading/Game/IW5/AssetLoaders/AssetLoaderGfxImage.h
+++ src/ObjLoading/Game/IW5/AssetLoaders/AssetLoaderGfxImage.h
@@ -1,6 +1,6 @@
 #pragma once
-
 #include "AssetLoading/BasicAssetLoader.h"
+#include "AssetLoading/IAssetLoadingManager.h"
 #include "Game/IW5/IW5.h"
 #include "SearchPath/ISearchPath.h"
 
@@ -10,5 +10,8 @@ namespace IW5
     {
     public:
         _NODISCARD void* CreateEmptyAsset(const std::string& assetName, MemoryManager* memory) override;
+        _NODISCARD bool CanLoadFromRaw() const override;
+        bool
+            LoadFromRaw(const std::string& assetName, ISearchPath* searchPath, MemoryManager* memory, IAssetLoadingManager* manager, Zone* zone) const override;
     };
 } // namespace IW5
