2b1c048a4ad19ec42146861640bc7e48a1e07bfb
Jan <jan@laupetin.net>
2024-09-23 20:15:52 +0200

    chore: improve argument parser code

--- src/Utils/Utils/Arguments/ArgumentParser.h
+++ src/Utils/Utils/Arguments/ArgumentParser.h
@@ -7,19 +7,20 @@
 
 class ArgumentParser
 {
-    std::vector<const CommandLineOption*> m_command_line_options;
-    std::map<const CommandLineOption*, std::vector<std::string>> m_matched_options;
-    std::vector<std::string> m_matched_arguments;
-
 public:
     ArgumentParser(const CommandLineOption* const* options, size_t optionCount);
 
     bool ParseArguments(std::vector<std::string>& args);
     bool ParseArguments(int argc, const char** argv);
 
-    std::vector<std::string> GetArguments() const;
+    [[nodiscard]] std::vector<std::string> GetArguments() const;
 
-    bool IsOptionSpecified(const CommandLineOption* option);
-    std::string GetValueForOption(const CommandLineOption* option);
-    std::vector<std::string> GetParametersForOption(const CommandLineOption* option);
+    bool IsOptionSpecified(const CommandLineOption* option) const;
+    std::string GetValueForOption(const CommandLineOption* option) const;
+    std::vector<std::string> GetParametersForOption(const CommandLineOption* option) const;
+
+private:
+    std::vector<const CommandLineOption*> m_command_line_options;
+    std::map<const CommandLineOption*, std::vector<std::string>> m_matched_options;
+    std::vector<std::string> m_matched_arguments;
 };
