2182196730c7814a5af794c2d8ca548947d666ef
Jan <jan@laupetin.net>
2024-10-14 20:44:54 +0200

    feat: add ipak and iwd zone definition metadata to replace type

--- src/ZoneCommon/Parsing/ZoneDefinition/ZoneDefinitionParserState.h
+++ src/ZoneCommon/Parsing/ZoneDefinition/ZoneDefinitionParserState.h
@@ -4,6 +4,7 @@
 #include "Zone/Definition/ZoneDefinition.h"
 
 #include <memory>
+#include <optional>
 
 class ZoneDefinitionParserState
 {
@@ -12,6 +13,15 @@ public:
 
     void SetGame(GameId game);
 
+    void StartIPak(std::string ipakName);
+    void StartIwd(std::string iwdName);
+
+    void Finalize();
+
     const IAssetNameResolver* m_asset_name_resolver;
+
+    std::optional<ZoneDefinitionObjContainer> m_current_ipak;
+    std::optional<ZoneDefinitionObjContainer> m_current_iwd;
+
     std::unique_ptr<ZoneDefinition> m_definition;
 };
