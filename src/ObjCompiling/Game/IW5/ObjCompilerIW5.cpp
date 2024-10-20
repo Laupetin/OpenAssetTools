199cfd74c9fe8d9d89d8cf4003d50050e0ac3941
Jan <jan@laupetin.net>
2024-10-20 14:12:16 +0200

    chore: add ObjCompiling component

---
+++ src/ObjCompiling/Game/IW5/ObjCompilerIW5.cpp
@@ -0,0 +1,8 @@
+#include "ObjCompilerIW5.h"
+
+using namespace IW5;
+
+ObjCompilerResult ObjCompiler::CompileAssetForZone(AssetLoadingContext& context, asset_type_t assetType, const std::string& assetName) const
+{
+    return ObjCompilerResult::NO_COMPILATION_DONE;
+}
