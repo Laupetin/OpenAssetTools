b215b220188b3ddbe95fb3359b0ee78a32993103
GoastcraftHD <simon.ickler@gmail.com>
2025-04-07 22:34:21 +0200

    Fixed MapEnts dumping in wrong folder

--- src/ObjWriting/Game/T6/AssetDumpers/AssetDumperMapEnts.cpp
+++ src/ObjWriting/Game/T6/AssetDumpers/AssetDumperMapEnts.cpp
@@ -1,6 +1,5 @@
 #include "AssetDumperMapEnts.h"
 
-#include <format>
 #include <filesystem>
 
 using namespace T6;
@@ -15,13 +14,13 @@ void AssetDumperMapEnts::DumpAsset(AssetDumpingContext& context, XAssetInfo<MapE
     const auto* mapEnts = asset->Asset();
 
     std::filesystem::path mapEntsPath(mapEnts->name);
-    mapEntsPath.replace_extension("map");
+    mapEntsPath.replace_extension("ents");
 
-   const auto mapEntsFile = context.OpenAssetFile(std::format("maps/{}", mapEntsPath.filename().string()));
+    const auto mapEntsFile = context.OpenAssetFile(mapEntsPath.string());
 
     if (!mapEntsFile)
         return;
 
     auto& stream = *mapEntsFile;
-    stream.write(mapEnts->entityString, mapEnts->numEntityChars);
+    stream.write(mapEnts->entityString, mapEnts->numEntityChars - 1);
 }
