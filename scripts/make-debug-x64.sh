13b07c0fe439c6d9d6f371f26a42daca3246084e
Jan <jan@laupetin.net>
2025-04-20 17:21:53 +0200

    chore: replace x64 build scripts with generic ones

--- scripts/make-debug-x64.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Go to repository root
-cd "$(dirname "$0")/.." || exit 2
-
-make -C build -j$(nproc) config=debug_x64 all
--- scripts/make-release-x64.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Go to repository root
-cd "$(dirname "$0")/.." || exit 2
-
-echo "Start building with $(nproc) threads"
-make -C build -j$(nproc) config=release_x64 all
