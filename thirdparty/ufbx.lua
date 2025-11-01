a8a8085340eb2ba7730778df4a99f230dd5091a2
LJW-Dev <lukewaters757@gmail.com>
2025-11-01 18:30:08 +0800

    Updated ufbx premake lua to properly add it to the project

--- thirdparty/ufbx.lua
+++ thirdparty/ufbx.lua
@@ -9,6 +9,7 @@ function ufbx:include(includes)
 end
 
 function ufbx:link()
+	links:add(self:name())
 end
 
 function ufbx:use()
@@ -20,4 +21,22 @@ function ufbx:name()
 end
 
 function ufbx:project()
+	local folder = ThirdPartyFolder()
+	local includes = Includes:create()
+
+	project(self:name())
+        targetdir(TargetDirectoryLib)
+		location "%{wks.location}/thirdparty/%{prj.name}"
+		kind "StaticLib"
+		language "C++"
+		
+		files { 
+			path.join(folder, "ufbx/ufbx.h"), 
+			path.join(folder, "ufbx/ufbx.c") 
+		}
+		
+		self:include(includes)
+
+		-- Disable warnings. They do not have any value to us since it is not our code.
+		warnings "off"
 end
