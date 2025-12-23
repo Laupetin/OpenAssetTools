5f04decaccb433fd0216fc65e9a63cd6c2a9b63c
Jan Laupetin <jan@laupetin.net>
2025-12-23 12:49:49 +0100

    chore: add simple iw3 system test

--- test/Catch2Common/main.cpp
+++ test/Catch2Common/main.cpp
@@ -1,3 +1,5 @@
+#include "Utils/Logging/Log.h"
+
 #include <catch2/catch_session.hpp>
 #include <filesystem>
 #include <format>
@@ -7,6 +9,9 @@ namespace fs = std::filesystem;
 
 int main(const int argc, char* argv[])
 {
+    con::init();
+    con::set_log_level(con::LogLevel::DEBUG);
+
     const fs::path absoluteBinDir(fs::canonical(argv[0]).parent_path());
 
     const auto expectedLibDir = absoluteBinDir.parent_path().parent_path();
@@ -15,24 +20,24 @@ int main(const int argc, char* argv[])
 
     if (absoluteBinDir.filename() != "tests" || expectedLibDir.filename() != "lib" || expectedBuildDir.filename() != "build")
     {
-        std::cerr << std::format("Expected test binary to be in the folder it was compiled into (build/lib/?/tests) but was {}\n", absoluteBinDir.string());
-        std::cerr << "Please do not move test executable out of compilation folder\n";
+        con::error("Expected test binary to be in the folder it was compiled into (build/lib/?/tests) but was {}", absoluteBinDir.string());
+        con::error("Please do not move test executable out of compilation folder");
         return 1;
     }
 
     const auto expectedSrcDir = expectedRootDir / "src";
     if (!fs::is_directory(expectedSrcDir))
     {
-        std::cerr << std::format("Expected source directory to exist in {}, but it did not\n", expectedSrcDir.string());
-        std::cerr << "Please do not move test executable out of compilation folder\n";
+        con::error("Expected source directory to exist in {}, but it did not", expectedSrcDir.string());
+        con::error("Please do not move test executable out of compilation folder");
         return 1;
     }
 
     const auto expectedTestDir = expectedRootDir / "test";
     if (!fs::is_directory(expectedTestDir))
     {
-        std::cerr << std::format("Expected test directory to exist in {}, but it did not\n", expectedTestDir.string());
-        std::cerr << "Please do not move test executable out of compilation folder\n";
+        con::error("Expected test directory to exist in {}, but it did not", expectedTestDir.string());
+        con::error("Please do not move test executable out of compilation folder");
         return 1;
     }
 
---
+++ test/SystemTests/Game/IW3/SimpleZoneIW3.cpp
@@ -0,0 +1,45 @@
+#include "Linker.h"
+#include "OatTestPaths.h"
+#include "SystemTestsPaths.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <format>
+#include <memory>
+#include <string>
+
+using namespace std::literals;
+
+namespace
+{
+    TEST_CASE("Simple Zone(IW3)", "[iw3][system][simple]")
+    {
+        const auto assetSearchPath = oat::paths::GetSystemTestsDirectory() / "Game/IW3/simple";
+        const auto sourceSearchPath = oat::paths::GetSystemTestsDirectory() / "Game/IW3/simple";
+        const auto outputPath = oat::paths::GetTempDirectory("iw3_simple");
+
+        const char* argStrings[]{
+            "SystemTests", // bin
+            "--verbose",
+            "--asset-search-path",
+            assetSearchPath.c_str(),
+            "--source-search-path",
+            sourceSearchPath.c_str(),
+            "--output-folder",
+            outputPath.c_str(),
+            "SimpleZoneIW3",
+        };
+
+        LinkerArgs args;
+
+        bool shouldContinue = true;
+        const auto couldParseArgs = args.ParseArgs(std::extent_v<decltype(argStrings)>, argStrings, shouldContinue);
+
+        REQUIRE(couldParseArgs);
+        REQUIRE(shouldContinue);
+
+        const auto linker = Linker::Create(std::move(args));
+        const auto linkerResult = linker->Start();
+
+        REQUIRE(linkerResult);
+    }
+} // namespace
