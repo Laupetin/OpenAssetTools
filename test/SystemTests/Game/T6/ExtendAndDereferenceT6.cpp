bc3a4a76a813aeceb19a2c2fd81d49e5659c6c50
Jan Laupetin <jan@laupetin.net>
2025-12-24 10:05:02 +0100

    chore: move extend and dereference description into test source file

--- test/SystemTests/Game/T6/ExtendAndDereferenceT6.cpp
+++ test/SystemTests/Game/T6/ExtendAndDereferenceT6.cpp
@@ -1,3 +1,14 @@
+/*
+This makes sure that when reusing assets from an existing zone, asset dependencies that are references can be overwritten to be non-references.
+
+In this case:
+
+- The zone `ZoneWithTechsetT6` contains the techniqueset `trivial_floatz_2992w610`
+- The zone `ZoneWithMaterialT6` contains the material `test` and with a reference to its techniqueset `,trivial_floatz_2992w610`
+- The final zone `CombinedZoneT6` is built while loading both of the other fastfile and is expected to contain both the material `test` with a reference to a
+(actual asset, not a reference) techniqueset `trivial_floatz_2992w610`
+*/
+
 #include "Game/T6/GameAssetPoolT6.h"
 #include "Linker.h"
 #include "OatTestPaths.h"
