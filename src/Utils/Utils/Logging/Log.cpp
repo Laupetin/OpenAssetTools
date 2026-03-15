58ab46413a33d61eda2b5083cec56bee0fe21333
Jan Laupetin <jan@laupetin.net>
2026-03-15 23:06:56 +0100

    chore: prefix warn and error messages

--- src/Utils/Utils/Logging/Log.cpp
+++ src/Utils/Utils/Logging/Log.cpp
@@ -89,16 +89,16 @@ namespace con
     void _warn_internal(const std::string& str)
     {
         if (globalUseColor)
-            std::cout << std::format("\x1B[33m{}\x1B[0m\n", str);
+            std::cout << std::format("\x1B[33mWARN: {}\x1B[0m\n", str);
         else
-            std::cout << std::format("{}\n", str);
+            std::cout << std::format("WARN: {}\n", str);
     }
 
     void _error_internal(const std::string& str)
     {
         if (globalUseColor)
-            std::cerr << std::format("\x1B[31m{}\x1B[0m\n", str);
+            std::cerr << std::format("\x1B[31mERROR: {}\x1B[0m\n", str);
         else
-            std::cerr << std::format("{}\n", str);
+            std::cerr << std::format("ERROR: {}\n", str);
     }
 } // namespace con
