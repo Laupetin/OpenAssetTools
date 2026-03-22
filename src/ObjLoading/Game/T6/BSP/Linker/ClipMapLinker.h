b26a8d4d9c840844b7696f0249ee45615b831686
LJW-Dev <48092720+LJW-Dev@users.noreply.github.com>
2026-03-22 17:08:30 +0800

    feat: clipmap linker now has support for maps smaller than the minimum BSP node size

--- src/ObjLoading/Game/T6/BSP/Linker/ClipMapLinker.h
+++ src/ObjLoading/Game/T6/BSP/Linker/ClipMapLinker.h
@@ -33,7 +33,7 @@ namespace BSP
         size_t highestLeafObjectCount = 0;
         std::vector<size_t> partitionToMaterialMap;
         void addAABBTreeFromLeaf(clipMap_t* clipMap, BSPTree* tree, size_t* out_parentCount, size_t* out_parentStartIndex, int* out_treeContents);
-        int16_t loadBSPNode(clipMap_t* clipMap, BSPTree* tree);
+        int16_t loadBSPNode(clipMap_t* clipMap, BSPTree* tree, bool isRoot);
         bool loadBSPTree(clipMap_t* clipMap, BSPData* bsp);
         bool loadPartitions(clipMap_t* clipMap, BSPData* bsp);
         bool loadWorldCollision(clipMap_t* clipMap, BSPData* bsp);
