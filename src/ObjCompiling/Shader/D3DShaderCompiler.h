b283695e1c194668b95a3ce51a1fa56b0df561d7
Jan Laupetin <jan@laupetin.net>
2026-03-25 22:47:50 +0100

    chore: dynamically enable shader compilation if available

---
+++ src/ObjCompiling/Shader/D3DShaderCompiler.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "SearchPath/ISearchPath.h"
+#include "Utils/MemoryManager.h"
+#include "Utils/Result.h"
+
+#include <optional>
+#include <string>
+
+namespace shader
+{
+    class CompiledShader
+    {
+    public:
+        void* m_shader_bin;
+        size_t m_shader_size;
+    };
+
+    bool ShaderCompilationAvailable();
+
+    result::Expected<std::optional<CompiledShader>, std::string> CompileShader(
+        const std::string& shaderFile, const std::string& entryPoint, const std::string& target, bool debug, ISearchPath& searchPath, MemoryManager& memory);
+} // namespace shader
