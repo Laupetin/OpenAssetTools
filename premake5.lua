0fe8becd6d377bfefecadcbc560ca54cf5ab84f5
LJW-Dev <48092720+LJW-Dev@users.noreply.github.com>
2026-04-10 12:16:52 +0800

    chore: Remove unused dependency ufbx

--- premake5.lua
+++ premake5.lua
@@ -102,7 +102,6 @@ include "thirdparty/minizip.lua"
 include "thirdparty/salsa20.lua"
 include "thirdparty/webview.lua"
 include "thirdparty/zlib.lua"
-include "thirdparty/ufbx.lua"
 
 -- ThirdParty group: All projects that are external dependencies
 group "ThirdParty"
@@ -117,7 +116,6 @@ group "ThirdParty"
     minizip:project()
     salsa20:project()
     zlib:project()
-    ufbx:project()
     
     if _OPTIONS["modman"] then
         webview:project()
