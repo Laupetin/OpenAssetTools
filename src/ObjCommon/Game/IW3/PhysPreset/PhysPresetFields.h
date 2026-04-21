86cceae0ec4589aec2cedb49a12ae2934238df35
njohnson <gitea.nicholasjohnson.info>
2026-04-20 21:04:52 -0400

    Fix PhysPresetFields.

--- src/ObjCommon/Game/IW3/PhysPreset/PhysPresetFields.h
+++ src/ObjCommon/Game/IW3/PhysPreset/PhysPresetFields.h
@@ -6,13 +6,15 @@
 namespace IW3
 {
     inline cspField_t phys_preset_fields[]{
-        {"mass",                 offsetof(PhysPresetInfo, mass),                 CSPFT_FLOAT   },
-        {"bounce",               offsetof(PhysPresetInfo, bounce),               CSPFT_FLOAT   },
-        {"friction",             offsetof(PhysPresetInfo, friction),             CSPFT_FLOAT   },
-        {"isFrictionInfinity",   offsetof(PhysPresetInfo, isFrictionInfinity),   CSPFT_QBOOLEAN},
-        {"bulletForceScale",     offsetof(PhysPresetInfo, bulletForceScale),     CSPFT_FLOAT   },
-        {"explosiveForceScale",  offsetof(PhysPresetInfo, explosiveForceScale),  CSPFT_FLOAT   },
-        {"piecesSpreadFraction", offsetof(PhysPresetInfo, piecesSpreadFraction), CSPFT_FLOAT   },
-        {"piecesUpwardVelocity", offsetof(PhysPresetInfo, piecesUpwardVelocity), CSPFT_FLOAT   },
+        {"sndAliasPrefix",        offsetof(PhysPresetInfo, sndAliasPrefix),        CSPFT_STRING  },
+        {"mass",                  offsetof(PhysPresetInfo, mass),                  CSPFT_FLOAT   },
+        {"friction",              offsetof(PhysPresetInfo, friction),              CSPFT_FLOAT   },
+        {"isFrictionInfinity",    offsetof(PhysPresetInfo, isFrictionInfinity),    CSPFT_QBOOLEAN},
+        {"bounce",                offsetof(PhysPresetInfo, bounce),                CSPFT_FLOAT   },
+        {"bulletForceScale",      offsetof(PhysPresetInfo, bulletForceScale),      CSPFT_FLOAT   },
+        {"explosiveForceScale",   offsetof(PhysPresetInfo, explosiveForceScale),   CSPFT_FLOAT   },
+        {"piecesSpreadFraction",  offsetof(PhysPresetInfo, piecesSpreadFraction),  CSPFT_FLOAT   },
+        {"piecesUpwardVelocity",  offsetof(PhysPresetInfo, piecesUpwardVelocity),  CSPFT_FLOAT   },
+        {"tempDefaultToCylinder", offsetof(PhysPresetInfo, tempDefaultToCylinder), CSPFT_FLOAT   },
     };
 }
