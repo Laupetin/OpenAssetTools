1c73c444500fba3aa957c952f56b59f0f1b7405c
njohnson <gitea.nicholasjohnson.info>
2026-04-22 18:32:03 -0400

    Removed unsupported vals (tracer & vehicle) and un-needed vals (material).

--- src/ObjCommon/Game/IW3/ObjConstantsIW3.h
+++ src/ObjCommon/Game/IW3/ObjConstantsIW3.h
@@ -8,14 +8,9 @@ namespace IW3
 
     public:
         static constexpr const char* INFO_STRING_PREFIX_PHYS_PRESET = "PHYSIC";
-        static constexpr const char* INFO_STRING_PREFIX_TRACER = "TRACER";
-        static constexpr const char* INFO_STRING_PREFIX_VEHICLE = "VEHICLEFILE";
         static constexpr const char* INFO_STRING_PREFIX_WEAPON = "WEAPONFILE";
 
-        static constexpr const char* GDF_FILENAME_MATERIAL = "material.gdf";
         static constexpr const char* GDF_FILENAME_PHYS_PRESET = "physpreset.gdf";
-        static constexpr const char* GDF_FILENAME_TRACER = "tracer.gdf";
-        static constexpr const char* GDF_FILENAME_VEHICLE = "vehicle.gdf";
         static constexpr const char* GDF_FILENAME_WEAPON = "weapon.gdf";
     };
 } // namespace IW3
