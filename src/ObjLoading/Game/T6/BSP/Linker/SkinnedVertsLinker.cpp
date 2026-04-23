8fd7691a2d0f2a28821599ea33f4ceb242d7bf47
LJW-Dev <48092720+LJW-Dev@users.noreply.github.com>
2026-04-24 00:21:38 +0800

    feat: use xmodels to calculate skinnedverts

--- src/ObjLoading/Game/T6/BSP/Linker/SkinnedVertsLinker.cpp
+++ src/ObjLoading/Game/T6/BSP/Linker/SkinnedVertsLinker.cpp
@@ -12,10 +12,9 @@ namespace BSP
     SkinnedVertsDef* SkinnedVertsLinker::linkSkinnedVerts(BSPData* bsp)
     {
         // Pretty sure maxSkinnedVerts relates to the max amount of xmodel skinned verts a map will have
-        // But setting it to the world vertex count seems to work
         SkinnedVertsDef* skinnedVerts = m_memory.Alloc<SkinnedVertsDef>();
         skinnedVerts->name = m_memory.Dup("skinnedverts");
-        skinnedVerts->maxSkinnedVerts = static_cast<unsigned int>(bsp->gfxWorld.vertices.size());
+        skinnedVerts->maxSkinnedVerts = static_cast<unsigned int>(bsp->gfxWorld.xmodels.size() * 1000);
 
         return skinnedVerts;
     }
