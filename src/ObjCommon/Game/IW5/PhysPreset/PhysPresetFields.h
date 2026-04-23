cbf95a196eaf0afbe6aa00f1a04c15a8876fb730
njohnson <gitea.nicholasjohnson.info>
2026-04-22 21:56:15 -0400

    Add PhysPresetFields.

---
+++ src/ObjCommon/Game/IW5/PhysPreset/PhysPresetFields.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Game/IW5/IW5.h"
+
+namespace IW5
+{
+    inline const char* szPhysPresetScalingNames[]{
+        "linear",
+        "quadratic",
+    };
+
+    inline cspField_t phys_preset_fields[]{
+        {"name",                  offsetof(PhysPresetInfo, name),                  CSPFT_STRING  },
+        {"type",                  offsetof(PhysPresetInfo, type),                  CSPFT_INT     },
+        {"mass",                  offsetof(PhysPresetInfo, mass),                  CSPFT_FLOAT   },
+        {"bounce",                offsetof(PhysPresetInfo, bounce),                CSPFT_FLOAT   },
+        {"friction",              offsetof(PhysPresetInfo, friction),              CSPFT_FLOAT   },
+        {"bulletForceScale",      offsetof(PhysPresetInfo, bulletForceScale),      CSPFT_FLOAT   },
+        {"explosiveForceScale",   offsetof(PhysPresetInfo, explosiveForceScale),   CSPFT_FLOAT   },
+        {"sndAliasPrefix",        offsetof(PhysPresetInfo, sndAliasPrefix),        CSPFT_STRING  },
+        {"piecesSpreadFraction",  offsetof(PhysPresetInfo, piecesSpreadFraction),  CSPFT_FLOAT   },
+        {"piecesUpwardVelocity",  offsetof(PhysPresetInfo, piecesUpwardVelocity),  CSPFT_FLOAT   },
+        {"minMomentum",           offsetof(PhysPresetInfo, minMomentum),           CSPFT_FLOAT   },
+        {"maxMomentum",           offsetof(PhysPresetInfo, maxMomentum),           CSPFT_FLOAT   },
+        {"minPitch",              offsetof(PhysPresetInfo, minPitch),              CSPFT_FLOAT   },
+        {"maxPitch",              offsetof(PhysPresetInfo, maxPitch),              CSPFT_FLOAT   },
+        {"volumeType",            offsetof(PhysPresetInfo, volumeType),            PPFT_SCALING  },
+        {"pitchType",             offsetof(PhysPresetInfo, pitchType),             PPFT_SCALING  },
+        {"tempDefaultToCylinder", offsetof(PhysPresetInfo, tempDefaultToCylinder), CSPFT_QBOOLEAN},
+        {"perSurfaceSndAlias",    offsetof(PhysPresetInfo, perSurfaceSndAlias),    CSPFT_QBOOLEAN},
+    };
+}
+
+
