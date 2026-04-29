b986fe31dd11eb775b641f81eab72c52d9088316
njohnson <gitea.nicholasjohnson.info>
2026-04-28 20:29:02 -0400

    Add PhysPresetInfo and constants.

---
+++ src/ObjCommon/Game/T5/ObjConstantsT5.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace T5
+{
+    static constexpr auto INFO_STRING_PREFIX_PHYS_PRESET = "PHYSIC";
+
+    static constexpr auto GDF_FILENAME_PHYS_PRESET = "physpreset.gdf";
+} // namespace T5
