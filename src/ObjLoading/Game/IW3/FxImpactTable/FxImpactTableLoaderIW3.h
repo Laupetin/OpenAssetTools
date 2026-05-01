1efa08f2ebe90ee20462ff88f278ce7aebe0742f
njohnson <gitea.nicholasjohnson.info>
2026-05-01 14:25:42 -0400

    Add placeholder loading logic.

---
+++ src/ObjLoading/Game/IW3/FxImpactTable/FxImpactTableLoaderIW3.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Asset/IAssetCreator.h"
+#include "Game/IW3/IW3.h"
+#include "SearchPath/ISearchPath.h"
+#include "Utils/MemoryManager.h"
+
+#include <memory>
+
+namespace fx_impact_table
+{
+    std::unique_ptr<AssetCreator<IW3::AssetImpactFx>> CreateLoaderIW3(MemoryManager& memory, ISearchPath& searchPath, Zone& zone);
+} // namespace fx_impact_table
