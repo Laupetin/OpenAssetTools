cfe66896b3039b4464e65b32d0f55f187365a875
njohnson <gitea.nicholasjohnson.info>
2026-05-02 13:11:04 -0400

    Added assert for attenuation image. Do not dump lmapLookupStart.

--- src/ObjWriting/Game/IW5/LightDef/LightDefDumperIW5.cpp
+++ src/ObjWriting/Game/IW5/LightDef/LightDefDumperIW5.cpp
@@ -2,6 +2,8 @@
 
 #include "LightDef/LightDefCommon.h"
 
+#include <assert.h>
+
 using namespace IW5;
 
 namespace light_def
@@ -10,22 +12,21 @@ namespace light_def
     {
         const auto* lightDef = asset.Asset();
         const auto assetFile = context.OpenAssetFile(GetFileNameForAsset(asset.m_name));
-
-        if (!assetFile || lightDef->attenuation.image == nullptr || lightDef->attenuation.image->name == nullptr || lightDef->cucoloris.image == nullptr
-            || lightDef->cucoloris.image->name == nullptr)
-            return;
-
         auto& stream = *assetFile;
 
+        assert(lightDef->attenuation.image != nullptr);
         const auto* attenuationImageName = lightDef->attenuation.image->name;
-        if (attenuationImageName[0] == ',')
+        if (attenuationImageName && attenuationImageName[0] == ',')
             attenuationImageName = &attenuationImageName[1];
 
-        const auto* cucolorisImageName = lightDef->cucoloris.image->name;
-        if (cucolorisImageName[0] == ',')
-            cucolorisImageName = &cucolorisImageName[1];
+        const auto* cucolorisImageName = "";
+        if (lightDef->cucoloris.image)
+        {
+            cucolorisImageName = lightDef->cucoloris.image->name;
+            if (cucolorisImageName && cucolorisImageName[0] == ',')
+                cucolorisImageName = &cucolorisImageName[1];
+        }
 
-        stream << lightDef->attenuation.samplerState << attenuationImageName << '\0' << lightDef->cucoloris.samplerState << cucolorisImageName
-               << static_cast<char>(lightDef->lmapLookupStart);
+        stream << lightDef->attenuation.samplerState << attenuationImageName << '\0' << lightDef->cucoloris.samplerState << cucolorisImageName;
     }
 } // namespace light_def
