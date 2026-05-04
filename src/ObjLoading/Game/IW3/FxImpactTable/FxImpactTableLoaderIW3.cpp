e5e9c3032fb91315f36853c1c1385dbe02b22a96
njohnson <gitea.nicholasjohnson.info>
2026-05-04 01:15:00 -0400

    Set up csv loader.

--- src/ObjLoading/Game/IW3/FxImpactTable/FxImpactTableLoaderIW3.cpp
+++ src/ObjLoading/Game/IW3/FxImpactTable/FxImpactTableLoaderIW3.cpp
@@ -6,16 +6,21 @@
 
 #include <format>
 #include <iostream>
+#include <Csv/CsvStream.h>
+#include <Game/IW3/FxImpactTable/FxImpactTableFields.h>
 
 using namespace IW3;
 
 namespace
 {
+    constexpr unsigned COL_COUNT_MIN = 3;
+
     class FxImpactTableLoader : public AssetCreator<AssetImpactFx>
     {
     public:
         FxImpactTableLoader(MemoryManager& memory, ISearchPath& searchPath, Zone& zone)
-            : m_search_path(searchPath)
+            : m_memory(memory),
+              m_search_path(searchPath)
         {
         }
 
@@ -26,12 +31,54 @@ namespace
             if (!file.IsOpen())
                 return AssetCreationResult::NoAction();
 
-            // TODO: Implement csv sound alias list loader
+            size_t entryCount = std::extent_v<decltype(fx_impact_type_names)>
+                                * (std::extent_v<decltype(fx_nonflesh_surface_type_names)> 
+                                    + std::extent_v<decltype(fx_flesh_surface_type_names)>);
 
-            return AssetCreationResult::NoAction();
+            auto* fxImpactTable = m_memory.Alloc<FxImpactTable>();
+            fxImpactTable->name = m_memory.Dup(assetName.c_str());
+            fxImpactTable->table = m_memory.Alloc<FxImpactEntry>(entryCount);
+            AssetRegistration<AssetImpactFx> registration(assetName, fxImpactTable);
+
+            const CsvInputStream csv(*file.m_stream);
+            std::vector<std::string> currentRow;
+            std::vector<FxImpactEntry> entries;
+            auto currentRowIndex = 0u;
+
+            while (csv.NextRow(currentRow))
+            {
+                currentRowIndex++;
+                CsvInputStream::PreprocessRow(currentRow);
+
+                if (CsvInputStream::RowIsEmpty(currentRow))
+                    continue;
+
+                if (currentRow.size() < COL_COUNT_MIN)
+                {
+                    con::error("{} Column count lower than min column count ({})", ErrorPrefix(assetName, currentRowIndex), COL_COUNT_MIN);
+                    return AssetCreationResult::Failure();
+                }
+
+                if (currentRowIndex - 1 < std::extent_v<decltype(fx_nonflesh_surface_type_names)>)
+                {
+                    fxImpactTable->table->nonflesh[currentRowIndex - 1]->name = currentRow[2].c_str();
+                }
+                else
+                {
+                    fxImpactTable->table->flesh[currentRowIndex - 1]->name = currentRow[2].c_str();
+                }
+            }
+
+            return AssetCreationResult::Success(context.AddAsset(std::move(registration)));
         }
 
     private:
+        static std::string ErrorPrefix(const std::string& assetName, const unsigned rowIndex)
+        {
+            return std::format("FxImpactTable \"{}\" Row {}:", assetName, rowIndex);
+        }
+
+        MemoryManager& m_memory;
         ISearchPath& m_search_path;
     };
 } // namespace
