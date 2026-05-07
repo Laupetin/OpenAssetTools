275ad66a59caf55053cf79fdcbc91127297028a8
njohnson <gitea.nicholasjohnson.info>
2026-05-06 20:34:39 -0400

    Allow tab character for abstract text dumper indent.

--- src/ObjWriting/Dumping/AbstractTextDumper.h
+++ src/ObjWriting/Dumping/AbstractTextDumper.h
@@ -7,7 +7,9 @@ class AbstractTextDumper
 protected:
     std::ostream& m_stream;
     size_t m_indent;
+    bool m_tab_indent;
 
+    void UseTabForIndent(bool tab_indent);
     void Indent() const;
     void IncIndent();
     void DecIndent();
