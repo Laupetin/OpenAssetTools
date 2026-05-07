dcda5233771201185f868ba1986d5b6ba1b69323
njohnson <gitea.nicholasjohnson.info>
2026-05-06 20:24:50 -0400

    Add constants for fxelemdef.

---
+++ src/ObjCommon/Game/IW3/Fx/FxElemDefConstantsIW3.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Game/IW3/IW3.h"
+
+#include <map>
+#include <utility>
+
+namespace IW3
+{
+    static const char* fx_elem_type_names[] = {"billboardSprite",
+                                                        "orientedSprite",
+                                                        "tail",
+                                                        "trail",
+                                                        "cloud",
+                                                        "model",
+                                                        "light",
+                                                        "light",
+                                                        "sound",
+                                                        "decal",
+                                                        "runner"
+                                                        };
+    } // namespace IW3
