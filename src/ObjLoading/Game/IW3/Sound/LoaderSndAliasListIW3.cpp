e53d633013673920aa7e3a3aabdccb9c83abbc63
njohnson <gitea.nicholasjohnson.info>
2026-05-07 23:50:17 -0400

    Start making csv loader for snt alias list.

--- src/ObjLoading/Game/IW3/Sound/LoaderSndAliasListIW3.cpp
+++ src/ObjLoading/Game/IW3/Sound/LoaderSndAliasListIW3.cpp
@@ -30,12 +30,85 @@ namespace
             if (!file.IsOpen())
                 return AssetCreationResult::NoAction();
 
-            // TODO: Implement csv sound alias list loader
+            auto* aliasList = m_memory.Alloc<snd_alias_list_t>();
+            aliasList->aliasName = m_memory.Dup(assetName.c_str());
+            aliasList->head = m_memory.Alloc<snd_alias_t>(aliasList->count);
+            AssetRegistration<AssetSound> registration(assetName, aliasList);
 
-            return AssetCreationResult::NoAction();
+            const CsvInputStream csv(*file.m_stream);
+            std::vector<std::string> currentRow;
+            std::vector<snd_alias_t> aliases;
+
+            // Skip header
+            csv.NextRow(currentRow);
+
+            auto currentRowIndex = 0u;
+            auto dataRowIndex = 0u;
+            while (csv.NextRow(currentRow))
+            {
+                snd_alias_t alias;
+
+                currentRowIndex++;
+                CsvInputStream::PreprocessRow(currentRow);
+
+                if (CsvInputStream::RowIsEmpty(currentRow))
+                    continue;
+
+                if (currentRow.size() < COL_COUNT_MIN)
+                {
+                    con::error("{} Column count lower than min column count ({})", ErrorPrefix(assetName, currentRowIndex), COL_COUNT_MIN);
+                    return AssetCreationResult::Failure();
+                }
+
+                if (!currentRow[0].empty())
+                {
+                    alias.aliasName = currentRow[0].c_str();
+                }
+
+                int sequence = std::stoi(currentRow[1]);
+                if (sequence == 0)
+                {
+                    alias.sequence = currentRowIndex - 1;
+                }
+                else
+                {
+                    alias.sequence = sequence;
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //auto makeFx = [&]() -> FxEffectDef*
+                //{
+                //    if (name.empty())
+                //        return nullptr;
+
+                //    return context.ForceLoadDependency<AssetFx>(name)->Asset();
+                //};
+
+                aliases.push_back(alias);
+            }
+
+            return AssetCreationResult::Success(context.AddAsset(std::move(registration)));
         }
 
     private:
+        static std::string ErrorPrefix(const std::string& assetName, const unsigned rowIndex)
+        {
+            return std::format("SoundAliasList \"{}\" Row {}:", assetName, rowIndex);
+        }
+
+        MemoryManager& m_memory;
         ISearchPath& m_search_path;
     };
 } // namespace
