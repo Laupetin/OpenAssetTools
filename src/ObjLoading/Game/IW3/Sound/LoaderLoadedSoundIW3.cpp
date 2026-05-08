de41cd4155c0ac95e1b58c2f5e4b57fdfb24c994
njohnson <gitea.nicholasjohnson.info>
2026-05-08 14:56:23 -0400

    Add LoadedSound loader.

---
+++ src/ObjLoading/Game/IW3/Sound/LoaderLoadedSoundIW3.cpp
@@ -0,0 +1,48 @@
+#include "LoaderLoadedSoundIW3.h"
+
+#include "Sound/WavTypes.h"
+#include "Sound/WavWriter.h"
+#include "Utils/Logging/Log.h"
+
+#include <format>
+
+using namespace IW3;
+
+namespace
+{
+    void DumpWavPcm(const LoadedSound* asset, std::ostream& stream)
+    {
+        const WavWriter writer(stream);
+
+        const WavMetaData metaData{.channelCount = static_cast<unsigned>(asset->sound.info.channels),
+                                   .samplesPerSec = static_cast<unsigned>(asset->sound.info.rate),
+                                   .bitsPerSample = static_cast<unsigned>(asset->sound.info.bits)};
+
+        writer.WritePcmHeader(metaData, asset->sound.info.data_len);
+        writer.WritePcmData(asset->sound.data, asset->sound.info.data_len);
+    }
+} // namespace
+
+namespace sound
+{
+    void LoadedSoundDumperIW3::DumpAsset(AssetDumpingContext& context, const XAssetInfo<LoadedSound>& asset)
+    {
+        const auto* loadedSound = asset.Asset();
+        const auto assetFile = context.OpenAssetFile(std::format("sound/{}", asset.m_name));
+
+        if (!assetFile)
+            return;
+
+        auto& stream = *assetFile;
+        switch (static_cast<WavFormat>(loadedSound->sound.info.format))
+        {
+        case WavFormat::PCM:
+            DumpWavPcm(loadedSound, stream);
+            break;
+
+        default:
+            con::error("Unknown format {} for loaded sound: {}", loadedSound->sound.info.format, loadedSound->name);
+            break;
+        }
+    }
+} // namespace sound
---
+++ src/ObjLoading/Game/IW3/Sound/LoaderLoadedSoundIW3.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Asset/IAssetCreator.h"
+#include "Game/IW3/IW3.h"
+#include "SearchPath/ISearchPath.h"
+#include "Utils/MemoryManager.h"
+
+#include <memory>
+
+namespace loaded_sound
+{
+    std::unique_ptr<AssetCreator<IW3::AssetLoadedSound>> CreateLoaderIW3(MemoryManager& memory, ISearchPath& searchPath, Zone& zone);
+} // namespace loaded_sound
