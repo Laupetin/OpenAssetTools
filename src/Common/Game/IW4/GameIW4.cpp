72594ddc2f8e6e5c8701b0723952f497706a5de2
njohnson <gitea.nicholasjohnson.info>
2026-05-12 20:29:59 -0400

    Clang format.

--- src/Common/Game/IW4/GameIW4.cpp
+++ src/Common/Game/IW4/GameIW4.cpp
@@ -9,14 +9,14 @@ using namespace IW4;
 namespace
 {
     constexpr const char* ASSET_TYPE_NAMES[]{
-        "physpreset",  "physcollmap", "xanim",        "xmodelsurfs", "xmodel",
-        "material",    "pixelshader", "vertexshader", "vertexdecl",  "techniqueset",
+        "physpreset",  "physcollmap", "xanim",        "xmodelsurfs",  "xmodel",
+        "material",    "pixelshader", "vertexshader", "vertexdecl",   "techniqueset",
         "image",       "sound",       "soundcurve",   "loaded_sound", "clipmap_unused",
-        "clipmap",     "comworld",    "gameworldsp",  "gameworldmp", "mapents",
-        "fxworld",     "gfxworld",    "lightdef",     "uimap",       "font",
-        "menulist",    "menu",        "localize",     "weapon",      "snddriverglobals",
-        "fx",          "impactfx",    "aitype",       "mptype",      "character",
-        "xmodelalias", "rawfile",     "stringtable",  "leaderboard", "structureddatadef",
+        "clipmap",     "comworld",    "gameworldsp",  "gameworldmp",  "mapents",
+        "fxworld",     "gfxworld",    "lightdef",     "uimap",        "font",
+        "menulist",    "menu",        "localize",     "weapon",       "snddriverglobals",
+        "fx",          "impactfx",    "aitype",       "mptype",       "character",
+        "xmodelalias", "rawfile",     "stringtable",  "leaderboard",  "structureddatadef",
         "tracer",      "vehicle",     "addonmapents",
     };
     static_assert(std::extent_v<decltype(ASSET_TYPE_NAMES)> == ASSET_TYPE_COUNT);
