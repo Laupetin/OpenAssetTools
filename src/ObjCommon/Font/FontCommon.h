15549133cdbba605a0cfc6ee75a495f650a00f7f
Jan Laupetin <jan@laupetin.net>
2026-07-03 00:55:19 +0200

    feat: encode printable letters as string in json
    Co-authored-by: hindercanrun <109132519+meowica@users.noreply.github.com>

--- src/ObjCommon/Font/FontCommon.h
+++ src/ObjCommon/Font/FontCommon.h
@@ -5,4 +5,9 @@
 namespace font
 {
     std::string GetJsonFileNameForAssetName(const std::string& assetName);
+
+    bool IsPrintableLetter(unsigned letter);
+
+    std::string LetterToString(unsigned letter);
+    unsigned StringToLetter(const std::string& str);
 } // namespace font
