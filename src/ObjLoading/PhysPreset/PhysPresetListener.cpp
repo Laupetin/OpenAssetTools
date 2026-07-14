0b3eb7ba033f8a872afb9b5aa5a9de6aaf27a702
njohnson <gitea.nicholasjohnson.info>
2026-07-14 18:53:35 -0400

    Re-generate for common parser instead of iw3 one.

--- src/ObjLoading/PhysPreset/PhysPresetListener.cpp
+++ src/ObjLoading/PhysPreset/PhysPresetListener.cpp
@@ -1,5 +1,5 @@
 
-// Generated from E:/Projects/VS/OpenAssetTools/src/ObjLoading/Game/IW3/PhysPreset/PhysPreset.g4 by ANTLR 4.13.2
+// Generated from E:/Projects/VS/OpenAssetTools/src/ObjLoading/PhysPreset/PhysPreset.g4 by ANTLR 4.13.2
 
 
 #include "PhysPresetListener.h"
