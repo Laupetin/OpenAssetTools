2c5eef91c67e372b5c207fe2e4bdfde0f9899c88
njohnson <gitea.nicholasjohnson.info>
2026-07-14 17:51:12 -0400

    Change IW3 PhysPreset loader to use ANTLR4 ast.

--- src/ObjLoading/Game/IW3/PhysPreset/RawLoaderPhysPresetIW3.cpp
+++ src/ObjLoading/Game/IW3/PhysPreset/RawLoaderPhysPresetIW3.cpp
@@ -1,25 +1,93 @@
 #include "RawLoaderPhysPresetIW3.h"
 
 #include "Game/IW3/IW3.h"
-#include "Game/IW3/ObjConstantsIW3.h"
-#include "InfoString/InfoString.h"
-#include "InfoStringLoaderPhysPresetIW3.h"
 #include "PhysPreset/PhysPresetCommon.h"
+#include "PhysPresetLexer.h"
+#include "PhysPresetParser.h"
 #include "Utils/Logging/Log.h"
 
+#include <antlr4-runtime.h>
 #include <format>
-#include <iostream>
+#include <limits>
+#include <string>
 
 using namespace IW3;
 
 namespace
 {
+    class PhysPresetErrorListener final : public antlr4::BaseErrorListener
+    {
+    public:
+        explicit PhysPresetErrorListener(const std::string& fileName)
+            : m_file_name(fileName),
+              m_has_errors(false)
+        {
+        }
+
+        void syntaxError(antlr4::Recognizer*, antlr4::Token*, size_t line, size_t charPos, const std::string& msg, std::exception_ptr) override
+        {
+            con::error("{}:{}:{}: {}", m_file_name, line, charPos, msg);
+            m_has_errors = true;
+        }
+
+        bool HasErrors() const
+        {
+            return m_has_errors;
+        }
+
+    private:
+        std::string m_file_name;
+        bool m_has_errors;
+    };
+
+    bool FillPhysPreset(PhysPreset& physPreset,
+                        const std::string& assetName,
+                        PhysPresetParser::PhysPresetContext* tree,
+                        MemoryManager& memory)
+    {
+        physPreset.name = memory.Dup(assetName.c_str());
+
+        float friction = 0.0f;
+        bool isFrictionInfinity = false;
+
+        for (auto* pair : tree->pair())
+        {
+            const auto key = pair->key()->getText();
+            const auto value = pair->value()->getText();
+
+            if (key == "mass")
+                physPreset.mass = std::stof(value);
+            else if (key == "bounce")
+                physPreset.bounce = std::stof(value);
+            else if (key == "friction")
+                friction = std::stof(value);
+            else if (key == "isFrictionInfinity")
+                isFrictionInfinity = value != "0";
+            else if (key == "bulletForceScale")
+                physPreset.bulletForceScale = std::stof(value);
+            else if (key == "explosiveForceScale")
+                physPreset.explosiveForceScale = std::stof(value);
+            else if (key == "sndAliasPrefix")
+                physPreset.sndAliasPrefix = memory.Dup(value.c_str());
+            else if (key == "piecesSpreadFraction")
+                physPreset.piecesSpreadFraction = std::stof(value);
+            else if (key == "piecesUpwardVelocity")
+                physPreset.piecesUpwardVelocity = std::stof(value);
+            else if (key == "tempDefaultToCylinder")
+                physPreset.tempDefaultToCylinder = value != "0";
+        }
+
+        physPreset.friction = isFrictionInfinity ? std::numeric_limits<float>::max() : friction;
+
+        return true;
+    }
+
     class RawLoaderPhysPreset final : public AssetCreator<AssetPhysPreset>
     {
     public:
-        RawLoaderPhysPreset(MemoryManager& memory, ISearchPath& searchPath, Zone& zone)
-            : m_search_path(searchPath),
-              m_info_string_loader(memory, zone)
+        RawLoaderPhysPreset(MemoryManager& memory, ISearchPath& searchPath)
+            : m_memory(memory),
+              m_search_path(searchPath)
         {
         }
 
@@ -30,19 +98,40 @@ namespace
             if (!file.IsOpen())
                 return AssetCreationResult::NoAction();
 
-            InfoString infoString;
-            if (!infoString.FromStream(INFO_STRING_PREFIX_PHYS_PRESET, *file.m_stream))
+            antlr4::ANTLRInputStream input(*file.m_stream);
+            PhysPresetLexer lexer(&input);
+            antlr4::CommonTokenStream tokens(&lexer);
+            PhysPresetParser parser(&tokens);
+
+            PhysPresetErrorListener errorListener(fileName);
+            lexer.removeErrorListeners();
+            lexer.addErrorListener(&errorListener);
+            parser.removeErrorListeners();
+            parser.addErrorListener(&errorListener);
+
+            auto* tree = parser.physPreset();
+
+            if (errorListener.HasErrors())
             {
-                con::error("Could not parse as info string file: \"{}\"", fileName);
+                con::error("Failed to parse phys preset: \"{}\"", fileName);
                 return AssetCreationResult::Failure();
             }
 
-            return m_info_string_loader.CreateAsset(assetName, infoString, context);
+            auto* physPreset = m_memory.Alloc<PhysPreset>();
+            AssetRegistration<AssetPhysPreset> registration(assetName, physPreset);
+
+            if (!FillPhysPreset(*physPreset, assetName, tree, m_memory))
+            {
+                con::error("Failed to convert phys preset: \"{}\"", assetName);
+                return AssetCreationResult::Failure();
+            }
+
+            return AssetCreationResult::Success(context.AddAsset(std::move(registration)));
         }
 
     private:
+        MemoryManager& m_memory;
         ISearchPath& m_search_path;
-        phys_preset::InfoStringLoaderIW3 m_info_string_loader;
     };
 } // namespace
 
@@ -50,6 +139,6 @@ namespace phys_preset
 {
     std::unique_ptr<AssetCreator<AssetPhysPreset>> CreateRawLoaderIW3(MemoryManager& memory, ISearchPath& searchPath, Zone& zone)
     {
-        return std::make_unique<RawLoaderPhysPreset>(memory, searchPath, zone);
+        return std::make_unique<RawLoaderPhysPreset>(memory, searchPath);
     }
 } // namespace phys_preset
