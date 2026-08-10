d8e00b1ae672ac12c5d12392d5f12ef587b977cd
Michael Oliver <michael.oliver_@outlook.com>
2026-08-10 14:17:42 +0100

    fix: extend `utils::GetExecutablePath()` with apple specific support

--- src/Utils/Utils/PathUtils.cpp
+++ src/Utils/Utils/PathUtils.cpp
@@ -7,6 +7,8 @@
 #elif defined(__linux__)
 #include <limits.h>
 #include <unistd.h>
+#elif defined(__APPLE__)
+#include <mach-o/dyld.h>
 #endif
 
 namespace utils
@@ -20,6 +22,17 @@ namespace utils
         char result[PATH_MAX];
         const auto count = readlink("/proc/self/exe", result, PATH_MAX);
         return std::string(result, (count > 0) ? count : 0);
+#elif defined(__APPLE__)
+        uint32_t size = 0;
+        _NSGetExecutablePath(nullptr, &size);
+
+        std::string result(size, '\0');
+        if (_NSGetExecutablePath(result.data(), &size) != 0)
+        {
+            throw std::runtime_error("Could not determine executable path");
+        }
+
+        return result.c_str();
 #else
         throw std::runtime_error("Unknown platform for executable path");
 #endif
