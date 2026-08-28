e39d5f3690e67a742b304fa9761c8f744a2a7dd1
Michael Oliver <michael.oliver_@outlook.com>
2026-08-28 08:56:09 +0100

    refactor: align IW3 menu dumpers with IW4

--- src/ObjWriting/Game/IW3/Menu/MenuDumperIW3.cpp
+++ src/ObjWriting/Game/IW3/Menu/MenuDumperIW3.cpp
@@ -14,10 +14,10 @@ namespace
     {
         const auto menuDumpingState = zoneState->m_menu_dumping_state_map.find(asset.Asset());
 
-        if (menuDumpingState != zoneState->m_menu_dumping_state_map.end())
-            return menuDumpingState->second.m_path;
+        if (menuDumpingState == zoneState->m_menu_dumping_state_map.end())
+            return "ui_mp/" + std::string(asset.Asset()->window.name) + ".menu";
 
-        return "ui_mp/" + std::string(asset.Asset()->window.name) + ".menu";
+        return menuDumpingState->second.m_path;
     }
 } // namespace
 
@@ -25,22 +25,27 @@ namespace menu
 {
     void MenuDumperIW3::DumpAsset(AssetDumpingContext& context, const XAssetInfo<AssetMenu::Type>& asset)
     {
+        const auto* menu = asset.Asset();
         auto* zoneState = context.GetZoneAssetDumperState<MenuDumpingZoneState>();
 
         if (!ObjWriting::ShouldHandleAssetType(ASSET_TYPE_MENULIST))
         {
-            const auto menuListAssets = context.m_zone.m_pools.PoolAssets<AssetMenuList>();
-            for (const auto* menuListAsset : menuListAssets)
+            // Make sure menu paths based on menu lists are created
+            auto menuListAssets = context.m_zone.m_pools.PoolAssets<AssetMenuList>();
+            for (auto* menuListAsset : menuListAssets)
                 CreateDumpingStateForMenuListIW3(zoneState, menuListAsset->Asset());
         }
 
-        const auto assetFile = context.OpenAssetFile(GetPathForMenu(zoneState, asset));
+        const auto menuFilePath = GetPathForMenu(zoneState, asset);
+        const auto assetFile = context.OpenAssetFile(menuFilePath);
+
         if (!assetFile)
             return;
 
-        const auto menuWriter = CreateMenuWriterIW3(*assetFile);
+        auto menuWriter = CreateMenuWriterIW3(*assetFile);
+
         menuWriter->Start();
-        menuWriter->WriteMenu(*asset.Asset());
+        menuWriter->WriteMenu(*menu);
         menuWriter->End();
     }
 } // namespace menu
--- src/ObjWriting/Game/IW3/Menu/MenuListDumperIW3.cpp
+++ src/ObjWriting/Game/IW3/Menu/MenuListDumperIW3.cpp
@@ -10,26 +10,14 @@ using namespace IW3;
 
 namespace
 {
-    std::string PathForMenu(const std::string& menuListParentPath, const menuDef_t* menu)
-    {
-        auto* menuName = menu->window.name;
-        if (!menuName)
-            return {};
-
-        if (menuName[0] == ',')
-            menuName++;
-
-        return menuListParentPath + menuName + ".menu";
-    }
-
-    void DumpMenus(menu::IWriterIW3& writer, menu::MenuDumpingZoneState* zoneState, const MenuList* menuList)
+    void DumpMenus(menu::IWriterIW3& menuDumper, menu::MenuDumpingZoneState* zoneState, const MenuList* menuList)
     {
         if (!menuList->menus)
             return;
 
-        for (auto menuIndex = 0; menuIndex < menuList->menuCount; menuIndex++)
+        for (auto menuNum = 0; menuNum < menuList->menuCount; menuNum++)
         {
-            const auto* menu = menuList->menus[menuIndex];
+            const auto* menu = menuList->menus[menuNum];
             if (!menu)
                 continue;
 
@@ -37,11 +25,25 @@ namespace
             if (menuDumpingState == zoneState->m_menu_dumping_state_map.end())
                 continue;
 
+            // If the menu was embedded directly as menu list write its data in the menu list file
             if (menuDumpingState->second.m_alias_menu_list == menuList)
-                writer.WriteMenu(*menu);
+                menuDumper.WriteMenu(*menu);
             else
-                writer.IncludeMenu(menuDumpingState->second.m_path);
+                menuDumper.IncludeMenu(menuDumpingState->second.m_path);
         }
+    }
+
+    std::string PathForMenu(const std::string& menuListParentPath, const menuDef_t* menu)
+    {
+        auto* menuAssetName = menu->window.name;
+
+        if (!menuAssetName)
+            return {};
+
+        if (menuAssetName[0] == ',')
+            menuAssetName = &menuAssetName[1];
+
+        return menuListParentPath + menuAssetName + ".menu";
     }
 } // namespace
 
@@ -52,53 +54,63 @@ namespace menu
         if (!menuList || menuList->menuCount <= 0 || !menuList->menus || !menuList->name)
             return;
 
-        const fs::path menuListPath(menuList->name);
+        const std::string menuListName(menuList->name);
+        const fs::path p(menuListName);
         std::string parentPath;
-        if (menuListPath.has_parent_path())
-            parentPath = menuListPath.parent_path().generic_string() + "/";
+        if (p.has_parent_path())
+            parentPath = p.parent_path().generic_string() + "/";
 
-        for (auto menuIndex = 0; menuIndex < menuList->menuCount; menuIndex++)
+        for (auto i = 0; i < menuList->menuCount; i++)
         {
-            auto* menu = menuList->menus[menuIndex];
+            auto* menu = menuList->menus[i];
+
             if (!menu)
                 continue;
 
-            const auto menuPath = PathForMenu(parentPath, menu);
+            auto menuPath = PathForMenu(parentPath, menu);
             if (menuPath.empty())
                 continue;
 
-            const auto isEmbeddedMenu = menuPath == menuList->name;
-            const auto existingState = zoneState->m_menu_dumping_state_map.find(menu);
+            auto existingState = zoneState->m_menu_dumping_state_map.find(menu);
             if (existingState == zoneState->m_menu_dumping_state_map.end())
             {
-                zoneState->CreateMenuDumpingState(menu, menuPath, isEmbeddedMenu ? menuList : nullptr);
+                const auto isTheSameAsMenuList = menuPath == menuListName;
+                zoneState->CreateMenuDumpingState(menu, std::move(menuPath), isTheSameAsMenuList ? menuList : nullptr);
             }
-            else if (isEmbeddedMenu && !existingState->second.m_alias_menu_list)
+            else if (!existingState->second.m_alias_menu_list)
             {
-                existingState->second.m_alias_menu_list = menuList;
-                existingState->second.m_path = menuPath;
+                const auto isTheSameAsMenuList = menuPath == menuListName;
+                if (isTheSameAsMenuList)
+                {
+                    existingState->second.m_alias_menu_list = menuList;
+                    existingState->second.m_path = std::move(menuPath);
+                }
             }
         }
     }
 
     void MenuListDumperIW3::DumpAsset(AssetDumpingContext& context, const XAssetInfo<AssetMenuList::Type>& asset)
     {
+        const auto* menuList = asset.Asset();
         const auto assetFile = context.OpenAssetFile(asset.m_name);
+
         if (!assetFile)
             return;
 
         auto* zoneState = context.GetZoneAssetDumperState<MenuDumpingZoneState>();
-        const auto writer = CreateMenuWriterIW3(*assetFile);
 
-        writer->Start();
-        DumpMenus(*writer, zoneState, asset.Asset());
-        writer->End();
+        auto menuWriter = CreateMenuWriterIW3(*assetFile);
+
+        menuWriter->Start();
+        DumpMenus(*menuWriter, zoneState, menuList);
+        menuWriter->End();
     }
 
     void MenuListDumperIW3::Dump(AssetDumpingContext& context)
     {
         auto* zoneState = context.GetZoneAssetDumperState<MenuDumpingZoneState>();
-        const auto menuListAssets = context.m_zone.m_pools.PoolAssets<AssetMenuList>();
+
+        auto menuListAssets = context.m_zone.m_pools.PoolAssets<AssetMenuList>();
         for (const auto* asset : menuListAssets)
             CreateDumpingStateForMenuListIW3(zoneState, asset->Asset());
 
