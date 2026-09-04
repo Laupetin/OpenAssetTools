1588b4519a2164131bf212713fe182fba9359c1f
mo <michael.oliver_@outlook.com>
2026-09-04 17:06:13 +0100

    chore: add Windows formatting convenience scripts (#922)
    * chore: add Windows formatting convenience scripts
    
    * chore: use powershell scripts
    
    ---------
    
    Co-authored-by: Jan Laupetin <jan@laupetin.net>

---
+++ scripts/reformat-all.ps1
@@ -0,0 +1,35 @@
+#!powershell
+
+$ErrorActionPreference = "Stop"
+
+$repositoryRoot = Split-Path -Parent $PSScriptRoot
+$sourceDirectories = @(
+    (Join-Path $repositoryRoot "src")
+    (Join-Path $repositoryRoot "test")
+)
+
+$clangFormat = if ($env:CLANG_FORMAT_BIN) {
+    $env:CLANG_FORMAT_BIN
+}
+else {
+    "clang-format"
+}
+
+$files = @(
+    Get-ChildItem -Path $sourceDirectories -Recurse -File |
+        Where-Object { $_.Extension -in ".h", ".cpp" }
+)
+
+# Stay comfortably below Windows' native process command-line limit.
+$batchSize = 100
+
+for ($offset = 0; $offset -lt $files.Count; $offset += $batchSize) {
+    $lastIndex = [Math]::Min($offset + $batchSize - 1, $files.Count - 1)
+    $batch = $files[$offset..$lastIndex].FullName
+
+    & $clangFormat --verbose -i $batch
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+}
