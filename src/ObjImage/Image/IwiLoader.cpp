28df35212390ac23994e95df52fad62425757eee
Jan Laupetin <jan@laupetin.net>
2026-09-06 22:28:37 +0200

    chore: add wavelet support for iwi8

--- src/ObjImage/Image/IwiLoader.cpp
+++ src/ObjImage/Image/IwiLoader.cpp
@@ -141,6 +141,25 @@ namespace
         };
     }
 
+    std::optional<IwiWaveletFormat> GetWaveletFormat8(const int8_t format)
+    {
+        switch (static_cast<iwi8::IwiFormat>(format))
+        {
+        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_RGBA:
+            return IwiWaveletFormat::RGBA;
+        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_RGB:
+            return IwiWaveletFormat::RGB;
+        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_LUMINANCE_ALPHA:
+            return IwiWaveletFormat::LUMINANCE_ALPHA;
+        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_LUMINANCE:
+            return IwiWaveletFormat::LUMINANCE;
+        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_ALPHA:
+            return IwiWaveletFormat::ALPHA;
+        default:
+            return std::nullopt;
+        }
+    }
+
     const ImageFormat* GetFormat8(int8_t format)
     {
         switch (static_cast<iwi8::IwiFormat>(format))
@@ -163,11 +182,6 @@ namespace
             return &format::R8_A8;
         case iwi8::IwiFormat::IMG_FORMAT_BITMAP_LUMINANCE:
             return &format::R8;
-        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_RGBA: // used
-        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_RGB:  // used
-        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_LUMINANCE_ALPHA:
-        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_LUMINANCE:
-        case iwi8::IwiFormat::IMG_FORMAT_WAVELET_ALPHA:
         case iwi8::IwiFormat::IMG_FORMAT_DXT3A_AS_LUMINANCE:
         case iwi8::IwiFormat::IMG_FORMAT_DXT5A_AS_LUMINANCE:
         case iwi8::IwiFormat::IMG_FORMAT_DXT3A_AS_ALPHA:
@@ -197,28 +211,18 @@ namespace
             return std::nullopt;
         }
 
-        const auto* format = GetFormat8(header.format);
-        if (format == nullptr)
-            return std::nullopt;
-
         const auto width = header.dimensions[0];
         const auto height = header.dimensions[1];
         const auto depth = header.dimensions[2];
         const auto hasMipMaps = !(header.flags & iwi8::IwiFlags::IMG_FLAG_NOMIPMAPS);
 
-        std::unique_ptr<Texture> texture;
+        TextureType textureType;
         if ((header.flags & iwi8::IwiFlags::IMG_FLAG_MAPTYPE_MASK) == iwi8::IwiFlags::IMG_FLAG_MAPTYPE_CUBE)
-        {
-            texture = std::make_unique<TextureCube>(format, width, height, hasMipMaps);
-        }
+            textureType = TextureType::T_CUBE;
         else if ((header.flags & iwi8::IwiFlags::IMG_FLAG_MAPTYPE_MASK) == iwi8::IwiFlags::IMG_FLAG_MAPTYPE_3D)
-        {
-            texture = std::make_unique<Texture3D>(format, width, height, depth, hasMipMaps);
-        }
+            textureType = TextureType::T_3D;
         else if ((header.flags & iwi8::IwiFlags::IMG_FLAG_MAPTYPE_MASK) == iwi8::IwiFlags::IMG_FLAG_MAPTYPE_2D)
-        {
-            texture = std::make_unique<Texture2D>(format, width, height, hasMipMaps);
-        }
+            textureType = TextureType::T_2D;
         else if ((header.flags & iwi8::IwiFlags::IMG_FLAG_MAPTYPE_MASK) == iwi8::IwiFlags::IMG_FLAG_MAPTYPE_1D)
         {
             con::error("Iwi has unsupported map type 1D");
@@ -230,28 +234,43 @@ namespace
             return std::nullopt;
         }
 
-        texture->Allocate();
-
-        auto currentFileSize = sizeof(iwi8::IwiHeader) + sizeof(IwiVersionHeader);
-        const auto mipMapCount = hasMipMaps ? texture->GetMipMapCount() : 1;
-
-        for (auto currentMipLevel = mipMapCount - 1; currentMipLevel >= 0; currentMipLevel--)
+        std::unique_ptr<Texture> texture;
+        if (const auto waveletFormat = GetWaveletFormat8(header.format))
         {
-            const auto sizeOfMipLevel = texture->GetSizeOfMipLevel(currentMipLevel) * texture->GetFaceCount();
-            currentFileSize += sizeOfMipLevel;
-
-            if (currentMipLevel < static_cast<int>(std::extent_v<decltype(iwi8::IwiHeader::fileSizeForPicmip)>)
-                && currentFileSize != header.fileSizeForPicmip[currentMipLevel])
-            {
-                con::error("Iwi has invalid file size for picmip {}", currentMipLevel);
+            texture = DecodeIwiWavelet(stream, textureType, width, height, hasMipMaps, *waveletFormat);
+            if (!texture)
                 return std::nullopt;
-            }
-
-            stream.read(reinterpret_cast<char*>(texture->GetBufferForMipLevel(currentMipLevel)), sizeOfMipLevel);
-            if (stream.gcount() != sizeOfMipLevel)
-            {
-                con::error("Unexpected eof of iwi in mip level {}", currentMipLevel);
+        }
+        else
+        {
+            const auto* format = GetFormat8(header.format);
+            if (format == nullptr)
                 return std::nullopt;
+
+            texture = Texture::CreateForType(textureType, format, width, height, depth, hasMipMaps);
+            texture->Allocate();
+
+            auto currentFileSize = sizeof(iwi8::IwiHeader) + sizeof(IwiVersionHeader);
+            const auto mipMapCount = hasMipMaps ? texture->GetMipMapCount() : 1;
+
+            for (auto currentMipLevel = mipMapCount - 1; currentMipLevel >= 0; currentMipLevel--)
+            {
+                const auto sizeOfMipLevel = texture->GetSizeOfMipLevel(currentMipLevel) * texture->GetFaceCount();
+                currentFileSize += sizeOfMipLevel;
+
+                if (currentMipLevel < static_cast<int>(std::extent_v<decltype(iwi8::IwiHeader::fileSizeForPicmip)>)
+                    && currentFileSize != header.fileSizeForPicmip[currentMipLevel])
+                {
+                    con::error("Iwi has invalid file size for picmip {}", currentMipLevel);
+                    return std::nullopt;
+                }
+
+                stream.read(reinterpret_cast<char*>(texture->GetBufferForMipLevel(currentMipLevel)), sizeOfMipLevel);
+                if (stream.gcount() != sizeOfMipLevel)
+                {
+                    con::error("Unexpected eof of iwi in mip level {}", currentMipLevel);
+                    return std::nullopt;
+                }
             }
         }
 
