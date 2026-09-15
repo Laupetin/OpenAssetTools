8f75d7ec954f455dba19a5a2442f2d81a438446c
Michael Oliver <michael.oliver_@outlook.com>
2026-09-15 15:45:17 +0100

    refactor: use named RGBA channels for FX colors

--- src/ObjCommon/Fx/FxCommon.h
+++ src/ObjCommon/Fx/FxCommon.h
@@ -4,5 +4,13 @@
 
 namespace fx
 {
+    struct RgbaColor
+    {
+        float r;
+        float g;
+        float b;
+        float a;
+    };
+
     std::string GetFileNameForAssetName(const std::string& assetName);
 }
